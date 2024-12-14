6ff3b6315a6be943af7ce0c492d64cfb6c503837
wightkid <103604852+wightkid@users.noreply.github.com>
2024-12-13 19:32:46 -0500

    Add custom move function Add boundaries for spawner

--- suika-clone/Scripts/Spawner.cs
+++ suika-clone/Scripts/Spawner.cs
@@ -3,13 +3,36 @@ using System;
 
 public partial class Spawner : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	float speed = 190;
+
+	[Export]
+	public float leftBound = 340f;
+
+    [Export]
+    public float rightBound = 800f;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		CustomMove(delta);
 	}
+
+	void CustomMove(double delta)
+	{
+		Translate(new Vector2(speed * (float)delta, 0));
+
+		if (Position.X > rightBound)
+		{
+			speed = -(Math.Abs(speed));
+		}
+
+        if (Position.X < leftBound)
+		{
+			speed = Math.Abs(speed);
+		}
+    }
 }
